d4dcb5fd11b7f7e1febe86c8d90a585ebfb1293a
yassinecheggar <cheggar13@gmail.com>
2021-03-01 01:13:54 +0100

    Signed-off-by: yassinecheggar <cheggar13@gmail.com>

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,23 +1,71 @@
-import React from 'react'
+import React from "react";
 import { Container } from "../../globalStyles";
-import {ImageContainer,Img,Title,TitleContainer} from './AboutElements';
+import {
+  ImageContainer,
+  Img,
+  Title,
+  TitleContainer,
+  Decscription,
+  HeadText,
+  TextContainer,
+  TextWrapper,
+} from "./AboutElements";
 
 function About() {
-    return (
-        <>
-            <ImageContainer>
-            <TitleContainer>
-            <Title>We make Mondays to Fridays exciting </Title>
-             
-                
-            </TitleContainer>
-                
-                <Img src="image/work.jpg"></Img>
-                
-            </ImageContainer>
+  return (
+    <>
+      <ImageContainer>
+        <TitleContainer>
+          <Title>We make Mondays to Fridays exciting </Title>
+        </TitleContainer>
 
-        </>
-    )
+        <Img src="image/work.jpg"></Img>
+      </ImageContainer>
+
+      <Container>
+        <TextWrapper>
+          <HeadText>SOCIÉTÉ</HeadText>
+          <Decscription>
+            MCDOSkills est une filiale du groupe MCDOS, constituée des élites
+            experts jeunes dynamiques, talentueux, ambitieux et toujours
+            disponibles à satisfaire vos ambitions.<br/> MCDOSkills offre via le
+            programme 360 Degree Des services dédiés aux parties (Entreprises,
+            jeunes diplômés, étudiants, associations, coopératives,..) qui
+            veulent créer ou améliorer leurs activités tout en visant une
+            excellence professionnelle, opérationnelle et durable.
+          </Decscription>
+        </TextWrapper>
+
+        <TextWrapper>
+          <HeadText>MISSION</HeadText>
+          <Decscription>
+          "Ready to change !" résume notre mission à savoir : l’apprentissage pratiques des métiers industriels et support créateurs de richesse, accompagnement sur le terrain des entreprises pour atteindre l’excellence opérationnelle, les doter des moyens humains, des moyens matériels optimaux, des techniques du marketing web, digitalisation, des techniques de communication, d’audit et de contrôle.
+          </Decscription>
+        </TextWrapper>
+
+        <TextWrapper>
+          <HeadText>VISION</HeadText>
+          <Decscription>
+          Créer des opportunités économiques durables, Word Class, et à Zéro mauvais stress pour chaque partie. 
+          </Decscription>
+        </TextWrapper>
+
+
+
+
+        <TextWrapper>
+          <HeadText>LES VALEURS</HeadText>
+          <Decscription>
+            Transmettre avec pragmatisme et humilité une réflexion, des méthodes
+            et des savoir-faire, enrichis par une expérience terrain.<br/> Faire
+            l’effort de comprendre vos réalités opérationnelles, afin que nos
+            actions de formation servent vos priorités. <br/>Faire preuve d’honnêteté
+            et de franchise dans toutes les étapes de la relation.
+          </Decscription>
+        </TextWrapper>
+      </Container>
+    </>
+  );
 }
 
 export default About;
--- src/components/About/AboutElements.js
+++ src/components/About/AboutElements.js
@@ -33,3 +33,38 @@ export const  TitleContainer = styled.div`
   right:10%;
   
 `;
+export const TextContainer = styled.div`
+display:flex;
+`;
+
+export const HeadText = styled.h1`
+   text-align:center;
+   color:#ff7315;
+   font-family: 'Cormorant Garamond', serif;
+   letter-spacing: 3px;
+`;
+
+export const Decscription = styled.p`
+     text-align: justify;
+  text-justify: inter-word;
+   margin-top:10px;
+   line-height: 2;
+   color:#232020;
+   font-family: 'Khula', sans-serif;
+   font-weight: 600;
+   padding-top:20px;
+   
+`;
+export const TextWrapper = styled.div`
+display:flex;
+flex-direction:column;
+margin-top:50px;
+margin-bottom:50px;
+padding-right:15%;
+padding-left:15%;
+
+@media screen and (max-width: 768px) {
+  padding-right:0%;
+padding-left:0%;
+  }
+`;
